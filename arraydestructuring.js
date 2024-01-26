a01e96aa95efdf1c788f0f3a32353aa1577b0734
midu-zack <midlajmidu0002@gmail.com>
2024-01-26 14:42:22 +0530

    day5

---
+++ arraydestructuring.js
@@ -0,0 +1,6 @@
+const array = ['midlaj','malappuram','plusTwo' ]
+
+
+const [name ,place, collification] = array
+
+console.log(name);
---
+++ objectdestructuring.js
@@ -0,0 +1,12 @@
+const object = {
+    name:'midlaj',
+    age:18,
+    place:'malappuram',
+    status:'single'
+}
+console.log(object.age);
+
+
+const {name,age,place,status }=object
+
+console.log(place);
