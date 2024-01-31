838188a0150744589ee9d16c0fa1328fcd865cb6
midu-zack <midlajmidu0002@gmail.com>
2024-01-31 16:27:49 +0530

    string methods

---
+++ failbetter.js
@@ -0,0 +1,80 @@
+// promise
+const testPromise = new Promise((resolve,reject)=>{
+    const data  =  'perfect'
+
+    if(data){
+        resolve(data)
+    }else{
+        reject('sorry this is not possible')
+    }
+})
+
+
+testPromise.then((data)=>{
+    console.log(data);
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+
+// closure
+
+function outer(){
+
+    let values = 900;
+
+    function inner(){
+
+        console.log(values);
+
+    }
+    inner()
+}
+
+outer()
+
+
+// try catch 
+
+let test = 'datas'
+
+if(test){
+    try{
+        console.log(test);
+    }
+    
+    catch(err){
+        console.log('sorry this is not possible ');
+        
+    }finally{
+        console.log('must proper working ');
+    }
+}
+
+
+
+
+//    splice 
+
+let array = [1,2,3,4,5,6,7,8,9] 
+
+let result = array.splice(5,2,100) //the array modify
+
+console.log(array);
+console.log(result);
+
+
+
+// slice
+let array2 = [1,2,3,4,5,6]
+
+let result2 = array2.slice(1,2) //the array not modify , only extract to the array 
+
+console.log(array2);
+console.log(result2);
+
+
+
+
+
---
+++ stringmethods.js
@@ -0,0 +1,46 @@
+// length 
+
+let str = 'MuhammedMidlaj'
+
+console.log(str.length);
+
+
+// chatAt
+let string = 'hello'
+console.log(string.charAt(2));
+
+
+// concact 
+
+console.log(str.concat(string));
+
+
+// indexof
+let test = 'zack how are you'
+console.log(test.indexOf("how"));
+
+
+// subString 
+console.log(test.substring(6,8));
+
+
+
+// toUppercase
+console.log(str.toUpperCase());
+
+
+
+// toLowercase
+console.log(str.toLocaleLowerCase());
+
+
+
+
+// split
+console.log(str.split());
+
+
+
+// replaced
+
+console.log(str.replace("MuhammedMidlaj","university" ));
